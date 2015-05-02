d5e7d18472ca0e8845860aecd721783a152a4ed8
zack <ciminera.zack@gmail.com>
2015-05-02 23:58:39 +0200

    nouvelle version java de l'analyse

--- Java/nanalyse/src/nanalyse/arguments.java
+++
@@ -1,9 +0,0 @@
-package nanalyse;
-
-public class arguments {
-	public static void main(String[] args) {
-		System.out.println("Il y a " + args.length + " arguments.");
-		for(int i = 0; i < args.length; i++) 
-			System.out.println("L'argument #" + (i+1) + " est " + args[i] );
-	}
-}
